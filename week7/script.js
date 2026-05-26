c14c988bfaa29b226efdd2c0cec988e2c9ddb2ce
Ritesh Aghade <aghaderitesh@gmail.com>
2026-05-26 14:48:03 +0530

    Week 7 enhance user experience completed

---
+++ week7/script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        alert("Plan Selected Successfully!");
+
+    });
+
+});
